81d3fe8b7e8387f0435bc19a6ffc92ba532fde26
Nazar Kranya <kranya.naz@gmail.com>
2019-02-28 23:27:00 +0200

    me

--- examples/route.js
+++
@@ -1,8 +0,0 @@
-router.post('/register', async (req, res) => {
-    try {
-      // do coding here
-    } catch (err) {
-        // your methods throw an error and catch it here
-      return res.status(err.status || 500).json(err);
-    }
-  });
