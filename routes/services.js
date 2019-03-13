62ff68741378d6dbed206ed6f26225a19ec294f2
Anna Yurchenko <44649397+anna-yurchenko@users.noreply.github.com>
2019-03-14 00:05:24 +0200

    Add files via upload

--- routes/services.js
+++ routes/services.js
@@ -1,1 +1,39 @@
-// 5
+const express = require('express');
+const fs = require('fs');
+
+const router = express.Router();
+
+router.post('/createNewService', function(req, res){
+    const servicesJson = JSON.parse(fs.readFileSync('./db/services.json', 'utf-8'))
+    let services = servicesJson.services;
+
+    let newService = {};
+    let newId = Math.random().toString();
+    newService.id = Number(newId.slice(2));
+    newService.title = req.body.title;
+    newService.userid = req.body.userid;
+    newService.description = req.body.description;
+    newService.time = req.body.time;
+
+    services.push(newService);
+    servicesJson.services = services;
+    fs.writeFileSync('./db/services.json', JSON.stringify(servicesJson));
+    return res.json(services);
+})
+
+router.get('/allServices', function(req, res){
+    const servicesJson = JSON.parse(fs.readFileSync('./db/services.json', 'utf-8'))
+    return res.json(servicesJson)
+})
+
+router.delete('/:id', function(req, res){
+    const servicesJson = JSON.parse(fs.readFileSync('./db/services.json', 'utf-8'))
+    let services = servicesJson.services;
+    let id = req.params.id.toString();
+    servicesJson.services = services.filter(u => u.id.toString() !== id)
+
+    fs.writeFileSync('./db/services.json', JSON.stringify(servicesJson));
+    return res.json(servicesJson.services);
+})
+
+module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,49 +3,59 @@ const fs = require('fs');
 
 const router = express.Router();
 
+router.post('/createNewUser', function(req, res){
+    const usersJson = JSON.parse(fs.readFileSync('./db/users.json', 'utf-8'))
+    let users = usersJson.users;
 
-// 1
-router.post('/createNewUser', function(req, res) {
-    const usersJSON = JSON.parse( fs.readFileSync('./db/users.json', 'utf-8'))
-    let users = usersJSON.users;
     let newUser = {};
+    let newId = Math.random().toString();
+    newUser.id = Number(newId.slice(2));
     newUser.name = req.body.name;
-    newUser.id = Math.random();
     newUser.city = req.body.city;
-    users.push( newUser )
-    usersJSON.users = users;
-    fs.writeFileSync('./db/users.json', JSON.stringify( usersJSON ) );
-    return res.json(users)
-});
 
-// 2
-router.get('/allUsers', function(req, res) {
-    const usersJSON = JSON.parse( fs.readFileSync('./db/users.json', 'utf-8'))
-    return res.json( usersJSON );
-});
+    users.push(newUser);
+    usersJson.users = users;
+    fs.writeFileSync('./db/users.json', JSON.stringify(usersJson));
+    return res.json(users);
+})
 
-// 3
-router.delete('/:id', function(req, res) {
-    const usersJSON = JSON.parse( fs.readFileSync('./db/users.json', 'utf-8'))
-    let users = usersJSON.users;
+router.get('/allUsers', function(req, res){
+    const usersJson = JSON.parse(fs.readFileSync('./db/users.json', 'utf-8'))
+    return res.json(usersJson)
+})
+
+router.delete('/:id', function(req, res){
+    const usersJson = JSON.parse(fs.readFileSync('./db/users.json', 'utf-8'))
+    let users = usersJson.users;
     let id = req.params.id.toString();
+    usersJson.users = users.filter(u => u.id.toString() !== id)
 
-    usersJSON.users = users.filter(u => u.id.toString() !== id);
+    fs.writeFileSync('./db/users.json', JSON.stringify(usersJson));
+    return res.json(usersJson.users);
+})
 
-    fs.writeFileSync('./db/users.json', JSON.stringify( usersJSON ) );
-    return res.json(usersJSON.users)
-});
-
-// 4
-router.put('/', function (req, res) {
-    // req.body.id should contain id of user for edit
-    // all other parameters will be data that should be added
-    // for example ---->>>> let {id, ...data} = req.body
-    // after it you should get data from JSON
-    // find user by id in this data
-    // update fields (replace the value)
-    // and save new data
-    // don't forget about validation, etc
-});
+router.put('/editUser', function(req, res){
+    const usersJson = JSON.parse(fs.readFileSync('./db/users.json', 'utf-8'));
+    let id = req.body.id;
+    let currentUser = {};
+    let user = usersJson.users;
+    for(let i=0; i<user.length; i++){
+        if(user[i].id == id){
+            currentUser.name = user[i].name;
+            currentUser.city = user[i].city;
+        }
+    }
+    let newData = req.body;
+    let updatedUser = {};
+    for (let key in currentUser){
+        if(currentUser[key] != newData[key]){
+            updatedUser[key] = newData[key]
+        }
+        else{
+            updatedUser[key] = currentUser[key]
+        }
+    }
+    return res.json(updatedUser);
+})
 
 module.exports = router;
